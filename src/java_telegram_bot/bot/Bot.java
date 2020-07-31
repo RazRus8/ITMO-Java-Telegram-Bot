05b2d12ef20bd9e4e567f6cdc1739cbe15de9513
Ruslan Razuvailo <razrus@hotmail.com>
2020-07-31 22:51:19 +0300

    Add multithreading.

--- src/java_telegram_bot/bot/Bot.java
+++ src/java_telegram_bot/bot/Bot.java
@@ -1,53 +1,65 @@
 package java_telegram_bot.bot;
 
-import java_telegram_bot.bot.userInputParse.UserInputParse;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Location;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import java_telegram_bot.bot.userInputParse.UserInputParse;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Bot extends TelegramLongPollingBot
 {
+    private void output(String message, Update update)
+    {
+        SendMessage sendMessage = new SendMessage();
+
+        if (message != null)
+        {
+            // message send to user
+            sendMessage.setText(message);
+        }
+
+        sendMessage.setChatId(update.getMessage().getChatId());
+
+        try
+        {
+            execute(sendMessage);
+        }
+        catch (TelegramApiException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void onUpdateReceived(Update update)
     {
-        String output;
-        SendMessage message = new SendMessage();
-
         System.out.println("\nUser (" + update.getMessage().getFrom().getUserName() + ") command: " + update.getMessage().getText());
 
         String command = update.getMessage().getText();
         Location location = update.getMessage().getLocation();
 
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(8);
+
         if (command != null)
         {
-            output = UserInputParse.parseCommand(update, command);
+            executor.submit(()->
+            {
+                output(UserInputParse.parseCommand(update, command), update);
+            });
         }
         else if (location != null)
         {
-            output = UserInputParse.parseLocation(update, location);
+            executor.submit(()->
+            {
+                output(UserInputParse.parseLocation(update, location), update);
+            });
         }
         else
         {
-            output = "Something went wrong.";
-        }
-
-        if (output != null)
-        {
-            // message send to user
-            message.setText(output);
-        }
-
-        message.setChatId(update.getMessage().getChatId());
-
-        try
-        {
-            execute(message);
-        }
-        catch (TelegramApiException e)
-        {
-            e.printStackTrace();
+            output("Something went wrong.", update);
         }
     }
 
